5cb4c421e5b59aa061f6e0b4d75bf6ece66d4f43
Amalia <amalialorenaferariu@gmail.com>
2021-10-20 14:42:10 +0200

    generated pasword js

---
+++ js/myscript.js
@@ -0,0 +1,19 @@
+// Chiedi all’utente il suo nome,
+
+let userName = prompt("what's your name?");
+console.log(userName);
+
+// poi chiedi il suo cognome,
+
+let userSurname = prompt("what's your surname?");
+console.log(userSurname);
+
+// poi chiedi il suo colore preferito
+
+let favColor = prompt("what's your favorite color?");
+console.log(userSurname);
+
+// Infine scrivi sulla pagina “La password assegnata è: ” nomecognomecolorepreferito21 
+
+let generatedPasword= userName + userSurname + favColor + 21
+document.getElementById("userPasword").innerHTML = "Your generated pasword is: " + generatedPasword
